3ef2649c602c75a017d45b9d748307b5b4fbb42d
Ikky <113713520+Zrokool@users.noreply.github.com>
2025-09-30 22:48:17 -0400

    Update AdminDeleteUser.php

--- application/evoting/AdminDeleteUser.php
+++ application/evoting/AdminDeleteUser.php
@@ -1,45 +1,36 @@
-<html>
-		<link rel="stylesheet" href="mrrobot-theme.css">
-	<head>
-		<title>User Deletion</title>
-	</head>
-		<h1>eVoting</h1>
-		<h3>Troubleshooting</h3>
-		<h4>Help find Username for client </h4>
-<!--button to go to another page-->
-<body>		 
-	<form action="DeleteUser.php" method="post">
-			Enter the Voter Citizen Number that you want to permenatly delete from the Database: <br/>
-			<br/><input type= "text" name="CitizenNum" /><br/><br/>
-			
-			Enter the Voter ID associated to the Voter you want to permenatly delete in the Database: <br/>
-			<br /><input type= "text" name="vID" /><br/><br/>
-		
-			
-			Enter the Voter Name : <br/>
-			<br /><input type= "text" name="vName" /><br /><br/>
-			
-			<input type= "submit" name= "submit" />
-			<br />*** Enter Correct CitizenNum and Voter ID<br />
-		</form>	
-		<form action="AdminCode.php">
-			Navigate to main page: 
-			<input type="submit" value="Back"/>
-		</form>
-</body>>	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="mrrobot-theme.css" />
+    <title>User Deletion</title>
+</head>
+<body>
+
+    <h1>eVoting</h1>
+    <h3>Troubleshooting</h3>
+    <h4>Help find Username for client</h4>
+    <!-- Button to go to another page -->
+
+    <form action="DeleteUser.php" method="post">
+        <label for="CitizenNum">Enter the Voter Citizen Number that you want to permanently delete from the Database:</label><br />
+        <input type="text" id="CitizenNum" name="CitizenNum" /><br /><br />
+
+        <label for="vID">Enter the Voter ID associated to the Voter you want to permanently delete in the Database:</label><br />
+        <input type="text" id="vID" name="vID" /><br /><br />
+
+        <label for="vName">Enter the Voter Name:</label><br />
+        <input type="text" id="vName" name="vName" /><br /><br />
+
+        <input type="submit" name="submit" value="Delete User" /><br />
+        <small>*** Enter Correct CitizenNum and Voter ID</small><br />
+    </form>
+
+    <form action="AdminCode.php" method="get">
+        Navigate to main page:
+        <input type="submit" value="Back" />
+    </form>
+
+</body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
