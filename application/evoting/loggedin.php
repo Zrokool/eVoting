2187530bb4418c670ce08b02fad7e5408c3992b2
Ikky <113713520+Zrokool@users.noreply.github.com>
2025-09-30 22:58:30 -0400

    Update loggedin.php

--- application/evoting/loggedin.php
+++ application/evoting/loggedin.php
@@ -1,57 +1,49 @@
-<html>
-	<head>
-		<title>eVoting</title>
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>eVoting</title>
+	<link rel="stylesheet" href="mrrobot-theme.css">
+</head>
+<body>
+	<h1>eVoting</h1>
+	<h3>Home Page</h3>
 
-		<h1>eVoting</h1>
-		<h3>Home Page</h3>
-<!--button to go to another page-->
-		<form action="accountinfo.php">
-			<input type="submit" value="Update Info">
-		</form>
-	</br>
-	</br>
-		<form action="StateInfo.php">
-			<input type="submit" value="Find State Id">
-		</form>
-	</br>
-	</br>
-		<form action="electioninfo2016.php">
-			<input type="submit" value="Vote Now">
-		</form>	
+	<form action="accountinfo.php">
+		<input type="submit" value="Update Info">
+	</form>
 
-	</br>
-	</br>
-		<form action="MyPollingStation.php">
-			<input type="submit" value="Find Polling Station">
-		</form>	
+	<form action="StateInfo.php">
+		<input type="submit" value="Find State Id">
+	</form>
 
-	</br>
-	</br>
-		<form action="VoterAnalytics.php">
-			<input type="submit" value="Election Stats">
-		</form>
-	</br>	
-	</br>
-		<form action="PostElectionAnalytics.php" method = "post">
-			<input type="submit"  value="Post Presidential Stats">
-		</form>		
-	</br>	
-	</br>	
-		<form action="StateBreakdown.php" method = "post">
-			<input type="submit"  value="State Stats">
-		</form>		
-	</br>	
-	</br>
-		<form action="StatePartyRatio.php" method = "post">
-			<input type="submit"  value="State Party Analysis">
-		</form>	
-	</br>
-	<br>	
-		<form action="Logout.php">
-			<input type="submit" value="Sign Out">
-		</form>
-		
-	</body>
+	<form action="electioninfo2016.php">
+		<input type="submit" value="Vote Now">
+	</form>
+
+	<form action="MyPollingStation.php">
+		<input type="submit" value="Find Polling Station">
+	</form>
+
+	<form action="VoterAnalytics.php">
+		<input type="submit" value="Election Stats">
+	</form>
+
+	<form action="PostElectionAnalytics.php" method="post">
+		<input type="submit" value="Post Presidential Stats">
+	</form>
+
+	<form action="StateBreakdown.php" method="post">
+		<input type="submit" value="State Stats">
+	</form>
+
+	<form action="StatePartyRatio.php" method="post">
+		<input type="submit" value="State Party Analysis">
+	</form>
+
+	<form action="Logout.php">
+		<input type="submit" value="Sign Out">
+	</form>
+</body>
 </html>
